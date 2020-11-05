45dca0af43be64e705f07a697584af5d2c5d666c
Gleyson Sampaio <gleysondev@gmail.com>
2020-11-05 20:25:32 -0300

    api de consulta de order, veriricar dependencia de pagemento

--- src/main/java/com/gama/passagens/amadeus/order/FlightOrderService.java
+++ src/main/java/com/gama/passagens/amadeus/order/FlightOrderService.java
@@ -8,9 +8,6 @@ import org.springframework.stereotype.Service;
 import com.amadeus.Amadeus;
 import com.amadeus.exceptions.ResponseException;
 import com.amadeus.resources.FlightOrder;
-import com.amadeus.resources.FlightPrice;
-import com.amadeus.resources.Traveler;
-import com.amadeus.resources.Traveler.Phone;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
@@ -38,7 +35,7 @@ public class FlightOrderService {
 		FlightOrderRequest req = gson.fromJson(jsonElement, FlightOrderRequest.class);
 		return req;
 	}
-	public String order(String json) throws RuntimeException {
+	public String postOrder(String json) throws RuntimeException {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		String response = null;
 		FlightOrder order = null;
@@ -56,106 +53,27 @@ public class FlightOrderService {
 		response = gson.toJson(order.getResponse().getResult());
 		return response;
 	}
-	public void order(Map orderRequest) throws Exception {
+	public String getOrder(String id) throws RuntimeException {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String response = null;
+		try {
+			//com.amadeus.booking.FlightOrder fo = new com.amadeus.booking.FlightOrder(amadeus,id);
+			//FlightOrder order = fo.get();
+			
+			FlightOrder order = amadeus.booking.flightOrder(id).get();
+			
+			if (order == null)
+				throw new RuntimeException("A passagem solicitada não existe.");
+			
+			if (order.getResponse().getStatusCode() != 200 && order.getResponse().getStatusCode() != 201)
+				throw new RuntimeException("" + order.getResponse().getStatusCode());
+			
+			response = gson.toJson(order.getResponse().getResult());
+		} catch (ResponseException e) {
+			throw new RuntimeException(e.getMessage().replaceAll("\n", " - "));
+		}
 		
-		FlightOrderRequest request = converter(orderRequest);
-		
-
-	    Traveler[] travelerArray = new Traveler[1];
-	    travelerArray[0] = request.getData().getTravelers().get(0);
-	    System.out.println(travelerArray[0]);
-	    
-	    
-	 // We price the 2nd flight of the list to confirm the price and the availability
-	    FlightPrice flightPricing = amadeus.shopping.flightOffersSearch.pricing.post(request.getData().getFlightOffers().get(0));
-
-	    // We book the flight previously priced
-	    FlightOrder order = amadeus.booking.flightOrders.post(flightPricing, travelerArray);
-	    System.out.println(order.getResponse());
-
-	    // Return CO2 Emission of the previously booked flight
-	    int weight = order.getFlightOffers()[0].getItineraries(
-	    )[0].getSegments()[0].getCo2Emissions()[0].getWeight();
-	    String unit = order.getFlightOffers()[0].getItineraries(
-	    )[0].getSegments()[0].getCo2Emissions()[0].getWeightUnit();
-
-
-		System.out.println();
+		return response;
 	}
 	
-	private static void traveller() {
-		Viajante traveler = new Viajante();
-
-	    traveler.setId("1");
-	    traveler.setDateOfBirth("1982-01-16");
-	    traveler.setName(traveler.new Name("JORGE", "GONZALES"));
-	    
-	    Traveler.Phone[] phone = new Phone[1];
-	    phone[0] = traveler.new Phone();
-	    phone[0].setCountryCallingCode("34");
-	    phone[0].setNumber("480080076");
-	    phone[0].setDeviceType("MOBILE");
-
-	    Viajante.Contato contact = traveler.new Contato();
-	    contact.setEmailAddress("jorge.gonzales833@telefonica.es");
-	    contact.setPhones(phone);
-	    
-	    traveler.setContact(contact);
-
-	    Viajante.Documento[] document = new Viajante.Documento[1];
-	    document[0] = traveler.new Documento();
-	    document[0].setDocumentType("PASSPORT");
-	    document[0].setBirthPlace("Madrid");
-	    document[0].setIssuanceLocation("Madrid");
-	    document[0].setIssuanceDate("2015-04-14");
-	    document[0].setNumber("00000000");
-	    document[0].setExpiryDate("2025-04-14");
-	    document[0].setIssuanceCountry("ES");
-	    document[0].setValidityCountry("ES");
-	    document[0].setNationality("ES");
-	    document[0].setHolder(true);
-	    traveler.setDocuments(document);
-	}
-}
-class Viajante extends Traveler{
-	public class Contato extends Traveler.Contact{
-		private String emailAddress;
-		public String getEmailAddress() {
-			return emailAddress;
-		}
-		public void setEmailAddress(String emailAddress) {
-			this.emailAddress = emailAddress;
-		}
-	}
-	public class Documento extends Traveler.Document{
-		private String birthPlace;
-		private String issuanceLocation;
-		private String issuanceDate;
-		private String validityCountry;
-		public String getBirthPlace() {
-			return birthPlace;
-		}
-		public void setBirthPlace(String birthPlace) {
-			this.birthPlace = birthPlace;
-		}
-		public String getIssuanceLocation() {
-			return issuanceLocation;
-		}
-		public void setIssuanceLocation(String issuanceLocation) {
-			this.issuanceLocation = issuanceLocation;
-		}
-		public String getIssuanceDate() {
-			return issuanceDate;
-		}
-		public void setIssuanceDate(String issuanceDate) {
-			this.issuanceDate = issuanceDate;
-		}
-		public String getValidityCountry() {
-			return validityCountry;
-		}
-		public void setValidityCountry(String validityCountry) {
-			this.validityCountry = validityCountry;
-		}
-		
-	}
 }
--- src/main/java/com/gama/passagens/project/controller/FlightController.java
+++ src/main/java/com/gama/passagens/project/controller/FlightController.java
@@ -3,12 +3,14 @@ package com.gama.passagens.project.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.amadeus.resources.FlightOrder;
 import com.gama.passagens.amadeus.flight.FlightSearch;
 import com.gama.passagens.amadeus.flight.FlightSearchService;
 import com.gama.passagens.amadeus.order.FlightOrderService;
@@ -34,7 +36,11 @@ public class FlightController {
     	
 	}
 	@PostMapping("/order")
-	public String order(@RequestBody String order) {
-		return orderService.order(order);
+	public String postOrder(@RequestBody String order) {
+		return orderService.postOrder(order);
+	}
+	@GetMapping("/order/get")
+	public String getOrder(@RequestParam("id") String id ) {
+		return orderService.getOrder(id);
 	}
 }
