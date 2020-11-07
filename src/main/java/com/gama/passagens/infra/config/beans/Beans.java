6196fcd8cb232d1d09d418598eceac6f4cecd3bc
Gleyson Sampaio <gleysondev@gmail.com>
2020-11-07 14:45:36 -0300

    ajustando as variaveis de servicos de api

--- src/main/java/com/gama/passagens/infra/config/beans/Beans.java
+++ src/main/java/com/gama/passagens/infra/config/beans/Beans.java
@@ -1,5 +1,6 @@
 package com.gama.passagens.infra.config.beans;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,6 +9,12 @@ import com.amadeus.Amadeus;
 
 @Configuration
 public class Beans {
+	@Value("${amadeus.id}")
+	private String amadeusId;
+	
+	@Value("${amadeus.secret}")
+	private String amadeusSecret;
+	
 	@Bean
     public BCryptPasswordEncoder encoder(){
         return new BCryptPasswordEncoder();
@@ -15,7 +22,7 @@ public class Beans {
 	@Bean
     public Amadeus amadeus(){
         return Amadeus
-				.builder("RZ3pbc22VwLAXtXqIU80DmT2hv1M2J8y", "zVyCOrYgU6ThENBK")
+				.builder(amadeusId, amadeusSecret)
 				.build();
     }
 }
