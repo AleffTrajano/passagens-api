4397d1c2e18bdf8656ab105e4ee4447b71613b13
Gleyson Sampaio <gleysondev@gmail.com>
2020-11-07 17:57:51 -0300

    war azure

--- src/main/java/com/PassagensApiApplication.java
+++ src/main/java/com/PassagensApiApplication.java
@@ -3,13 +3,14 @@ package com;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import com.gama.passagens.project.start.Start;
 
 @SpringBootApplication
 //@EnableFeignClients
-public class PassagensApiApplication{
+public class PassagensApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PassagensApiApplication.class, args);
