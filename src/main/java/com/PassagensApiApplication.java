ebb3902f6da19f79d8a575f2f5e78c42cc1ffb0e
Gleyson Sampaio <gleysondev@gmail.com>
2020-11-07 16:44:37 -0300

    gerando war

--- src/main/java/com/PassagensApiApplication.java
+++ src/main/java/com/PassagensApiApplication.java
@@ -1,25 +1,15 @@
 package com;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.gama.passagens.project.start.Start;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //@EnableFeignClients
-public class PassagensApiApplication {
+public class PassagensApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PassagensApiApplication.class, args);
 	}
 	
-	@Bean
-    public CommandLineRunner run( Start st) {
-        return args -> {
-        	st.init();
-        	
-        };
-    }
 }
